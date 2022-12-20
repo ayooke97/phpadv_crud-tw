9c64f7db69c041cb1afb3674d8a521ef337de7ed
ayooke97 <bakti12356@gmail.com>
2022-12-20 16:55:23 +0700

    gak tau lagi dah

--- perpustakaan.sql
+++ perpustakaan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 12, 2022 at 09:38 AM
+-- Generation Time: Dec 20, 2022 at 10:54 AM
 -- Server version: 10.4.25-MariaDB
 -- PHP Version: 8.1.10
 
@@ -32,17 +32,19 @@ CREATE TABLE `buku` (
   `judul_buku` varchar(60) NOT NULL,
   `penerbit` varchar(60) NOT NULL,
   `sinopsis` text NOT NULL,
-  `th_terbit_buku` year(4) NOT NULL
+  `th_terbit_buku` year(4) NOT NULL,
+  `cover_buku` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 --
 -- Dumping data for table `buku`
 --
 
-INSERT INTO `buku` (`id_buku`, `judul_buku`, `penerbit`, `sinopsis`, `th_terbit_buku`) VALUES
-(51, 'I Feel Bad About My Neck', 'Nora Ephron', 'anjayA', 2006),
-(52, 'AWOAKWOAKWOAKW', 'SDKFJKLSDJF', 'DSFHJKSDHFJKSDHFKJHSDJKFHKSJDFH', 2022),
-(53, 'bbbbbbbbbbbb', 'bss', 'dfgsdfgsdfgsdffggdf', 2022);
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `penerbit`, `sinopsis`, `th_terbit_buku`, `cover_buku`) VALUES
+(106, 'I Feel Bad About My Neckab', 'sdfsdf', 'AWOKAWOKOAWKOAWKOAWKAW', 2018, ''),
+(107, 'Buku sakti hacker', 'Entah Berantah ', 'RILIS BOSS', 2023, '478-mk ks.jpg'),
+(116, 'Kosong', '', '', 0000, '261-mk ks.jpg'),
+(117, 'Gaktau buku apaan', 'Orang Ketiga', 'Dah lah males', 2022, '541-mk ks.jpg');
 
 -- --------------------------------------------------------
 
@@ -69,7 +71,9 @@ INSERT INTO `user` (`id`, `username`, `email`, `password`, `first_name`, `mid_na
 (19, 'ayooke97', 'bakti12356@gmail.com', '$2y$10$mpK7G.dPdwi3AJTkQ2xdnOFPS01S.PwZAFf.OnhtvMIr05shW5u5i', 'Bakti', 'P', 'M', 'Rahasia'),
 (20, 'dma555', 'dimasanjaymabar@gmail.com', '$2y$10$Gq8hdVLg8v8tkq1U9pH6re.AczLFGvvmaL4RrqcCJBEGq0mpKKXUa', 'Dimas', 'Anjay', 'Mabar', 'Laki-Laki'),
 (21, 'awokawok97', 'baktiawokawok@gmail.com', '$2y$10$19DboOvdWMT4yJeM81cKu.BQWa28GZD5lxr4nHA3DVxrOaD/7LHOy', 'Awok', 'Awok', 'Awok', 'Rahasia'),
-(22, 'nocomment404', 'nocomment404@gmail.com', '$2y$10$LN87D2lLiFB0GeoZ6Dzrtur2p7vEyEmICn0Tk1QsqAf2NYcDxntNK', 'No', '', 'Comment', 'Rahasia');
+(22, 'nocomment404', 'nocomment404@gmail.com', '$2y$10$LN87D2lLiFB0GeoZ6Dzrtur2p7vEyEmICn0Tk1QsqAf2NYcDxntNK', 'No', '', 'Comment', 'Rahasia'),
+(23, '', '', '$2y$10$qBodNgvZ3Fw5jK0TLT6lJeQKhIAkb.OlxdSX2wluQqd5/GYaH8W6q', '', '', '', ''),
+(24, 'ngetesdoang123', 'ngetesdoang@gmail.com', '$2y$10$0npyc8/ximsk.YH9XCViKeAJSktCC8mns4PSP8ofNbmtWl403TMIa', 'Nge', 'Test', 'Doang', '');
 
 --
 -- Indexes for dumped tables
@@ -95,13 +99,13 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `buku`
 --
 ALTER TABLE `buku`
-  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=54;
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=118;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
